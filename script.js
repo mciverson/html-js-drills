7b8a7af13f1ebaf5620fbdeccad333484966c59f
mndlovu18 <mndlovu@brynmawr.edu>
2020-07-15 02:05:33 -0400

    js-drill

--- script.js
+++ script.js
@@ -306,7 +306,30 @@ function newElement() {
  *
  */
 
-// Your code goes here
+var minutesLabel = document.getElementById("minutes");
+var secondsLabel = document.getElementById("seconds");
+var totalSeconds = 0;
+setInterval(setTime, 1000);
+
+function setTime() {
+  ++totalSeconds;
+  secondsLabel.innerHTML = pad(totalSeconds % 60);
+  minutesLabel.innerHTML = pad(parseInt(totalSeconds / 60));
+}
+
+function pad(val) {
+  var valString = val + "";
+  if (valString.length < 2) {
+    return "0" + valString;
+  } else {
+    return valString;
+  }
+} 
+
+
+
+
+
 
 /****************************************
  * Section 6 - Your own section!        *
